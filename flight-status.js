fc7a92166ff6def1ec8f0207e0d769f6c557c037
Jared <38020231+Jared-Glenn@users.noreply.github.com>
2023-06-05 22:51:39 -0600

    Create flight-status.js

---
+++ flight-status.js
@@ -0,0 +1,20 @@
+//Adapted from Sololearn Practice on June 5, 2023
+
+function main() {
+    //take flight number and its status
+    var flightNumber = readLine();
+    var flightStatus = readLine();
+    
+    var flight1 = new Flight(flightNumber, flightStatus);
+    //assign a flight object to flight1 variable
+    
+    //output
+    console.log('The flight ' + flight1.number + ' is ' + flight1.status)
+    
+}
+
+function Flight(flightNumber, status) {
+    //fix the constructor
+    this.number = flightNumber;
+    this.status = status;
+};
