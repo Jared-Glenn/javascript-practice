95ca313f99b33c9bcb5ea5d05b606514e7ab6e90
Jared <38020231+Jared-Glenn@users.noreply.github.com>
2023-06-09 17:07:21 -0600

    Create 005-array-replacement.js

---
+++ 005-array-replacement.js
@@ -0,0 +1,11 @@
+// Adapted from Sololearn exercise on June 9, 2023.
+
+function main() {
+    var breakfasts = ['Cinnamon Doughnuts', 'Waffles', 'Granola', 'Chorizo Burrito', 'French Toast'];
+    var index = parseInt(readLine(), 10)
+   
+    //replace the corresponding element by "Fluffy Pancakes"
+    breakfasts[index] = "Fluffy Pancakes";
+    //output the menu to the console
+    console.log(breakfasts)
+}
